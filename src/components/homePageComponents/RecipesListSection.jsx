7dd6bc639d7560818269745b24dcaaebd5084870
Vlad19772001 <vlad19772001@gmail.com>
2023-10-10 21:06:46 +0200

    test2

--- src/components/homePageComponents/RecipesListSection.jsx
+++ src/components/homePageComponents/RecipesListSection.jsx
@@ -1,9 +1,12 @@
 import { RecipesListItem } from 'components/reusableComponents/RecipesListItem';
+import css from '../../styles/home.module.css'
+
+
 
 export const RecipesListSection = ({ data }) => {
   return (
     <section>
-      <h3>{data.title}</h3>
+      <h3 className={css.hero_title}>{data.title}</h3>
       <div style={{ display: 'flex' }}>
         {data.content.map((el, idx) => (
           <RecipesListItem key={idx} data={el} />
